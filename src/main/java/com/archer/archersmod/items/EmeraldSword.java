e1eede651bd1caf3f6d56381164a1e6cdce6301b
archersa <archersa@Archers-MacBook-Air.local>
2014-12-23 11:21:59 -0800

    Christmas Sword

--- src/main/java/com/archer/archersmod/items/EmeraldSword.java
+++ src/main/java/com/archer/archersmod/items/EmeraldSword.java
@@ -6,8 +6,14 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 public class EmeraldSword extends ItemSword {
 	public EmeraldSword(ToolMaterial material) {
@@ -19,12 +25,40 @@ public class EmeraldSword extends ItemSword {
 
 	@SideOnly(Side.CLIENT)
 	private IIcon[] icons;
+	private IIcon itemIcon2;
+	private IIcon itemIcon1;
 
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IIconRegister par1IconRegister) {
-		this.itemIcon = par1IconRegister.registerIcon(ArchersMod.modid + ':'
-				+ "eme	rald_sword");
+		this.itemIcon1 = par1IconRegister.registerIcon(ArchersMod.modid + ':'
+				+ "emerald_sword");
+		this.itemIcon2 = par1IconRegister.registerIcon(ArchersMod.modid + ':'
+				+ "emerald_sword_alt");
+		this.itemIcon = this.itemIcon1;
+	}
+
+	@Override
+	public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack) {
+		swingCounter++;
+		if (swingCounter % 2 == 0) {
+			swapIcon();
+		}
+		return super.onEntitySwing(entityLiving, stack);
+	}
+
+	int swingCounter;
+
+	private void swapIcon() {
+		if (this.itemIcon == this.itemIcon1) {
+			this.itemIcon = this.itemIcon2;
+		} else {
+			this.itemIcon = this.itemIcon1;
+		}
+	}
+
+	@Override
+	public boolean onDroppedByPlayer(ItemStack item, EntityPlayer player) {
+		swapIcon();
+		return super.onDroppedByPlayer(item, player);
 	}
 }
-
-
